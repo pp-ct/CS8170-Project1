e5fe58e2233d39ff174f4e4038e4be4e0d4a8a49
Clement <clem.essien@gmail.com>
2019-02-23 15:29:35 -0600

    code refactoring

--- Project-Code/src/script.py
+++ Project-Code/src/script.py
@@ -14,29 +14,30 @@ TARGET_DIR = "../data/target/" #place target fasta files here i.e. one fasta fil
 OUTPUT_DIR = "../data/msa/"
 
 
-def parse_result(msa_xml_file):
+def parse_result(msa_xml_file,file_no):
 	result=open(msa_xml_file,"r")
-	output+= OUTPUT_DIR+"msa.fasta";
+	output= OUTPUT_DIR+"msa"+str(file_no)+".fasta";
 	records= NCBIXML.parse(result)
 	item=next(records)
 	for alignment in item.alignments:
 		for hsp in alignment.hsps:
 			library.write_to_file(output, ">"+alignment.accession[0:4]+":"+alignment.accession[5]+"|PDBID|CHAIN|SEQUENCE","a")
 			library.write_to_file(output, textwrap.fill(hsp.query, 60), "a")
-		library.write_to_file(output+"msa.fasta", "", "a")
+		library.write_to_file(output, "", "a")
 
 def main():
 	for filename in os.listdir(TARGET_DIR):
-	    if filename.endswith(".fasta") : 
-	    	 target_file = TARGET_DIR+filename
+		file_no =0; 
+		if filename.endswith(".fasta") :
+	    	 file_no +=1;
+	    	 target_file = TARGET_DIR+filename;
 	    	 msa_file = OUTPUT_DIR+"msa.xml";
 	    	 record = SeqIO.read(target_file, format="fasta")
 	    	 result_handle = NCBIWWW.qblast("blastp", "pdbaa", record.seq)
 	    	 library.create_dir(OUTPUT_DIR)
 	    	 library.write_stream(msa_file, result_handle)
-	    	 parse_result(msa_file)
-	    else:
-	        continue
-
+	    	 parse_result(msa_file, file_no)
+	    	else:
+	        	continue
 
 main()
