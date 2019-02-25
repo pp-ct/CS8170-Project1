aaabf90e966a2ba4361aa0fe5a6b293cb8818520
Jeff Ruffolo <jarpqd@mail.missouri.edu>
2019-02-25 15:37:49 -0600

    added function for computing distances between CA atoms in PDB

--- Project-Code/lib/library.py
+++ Project-Code/lib/library.py
@@ -1,4 +1,6 @@
-import os;
+import os
+from Bio import PDB
+import numpy as np
 
 def write_to_file(file_name, data, mode):
 	with open(file_name, mode) as file:  
@@ -12,3 +14,22 @@ def write_stream(filename,data_stream):
 def create_dir(directory):
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+
+def load_ca_distnaces_from_pdb(path, length, chain_id=None):
+    parser = PDB.PDBParser()
+    chain = parser.get_structure(id='temp', file=path)[0]
+    if chain_id is not None:
+        chain = parser.get_structure(id='temp', file=path)[0][chain_id]
+    distance_matrix = np.zeros((length, length))
+
+    for r1, residue1 in enumerate(chain):
+        for r2, residue2 in enumerate(chain):
+            if residue1 != residue2:
+                # compute distance between CA atoms
+                try:
+                    distance = residue1['CA'] - residue2['CA']
+                    distance_matrix[r1][r2] = distance
+                except KeyError:
+                    continue
+
+    return distance_matrix
