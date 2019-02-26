40d17e27aa9a59c728de9e48f8f5a0137de5baf2
Jeff Ruffolo <jarpqd@mail.missouri.edu>
2019-02-25 21:19:58 -0600

    added function to generate query_hit_dict to Alignment object

--- Project-Code/src/Alignment.py
+++ Project-Code/src/Alignment.py
@@ -1,8 +1,36 @@
+
 class Alignment:
   
-  def __init__(self,hit_id, query_range, hit_range, query_hit_dict):
+  def __init__(self,hit_id, query_range, hit_range, query_seq, hit_seq, midline):
     self.hit_id = hit_id
     self.query_range = query_range 
     self.hit_range = hit_range
-    self.query_hit_dict = query_hit_dict
 
+    self.query_seq = query_seq
+    self.hit_seq = hit_seq
+    self.midline = midline
+
+    self.query_hit_dict = self.generate_query_hit_dict()
+
+
+  def generate_query_hit_dict(self):
+    query_hit_dict = {}
+    q_index, h_index = self.query_range[0], self.hit_range[0]
+    for q, h, m in zip(self.query_seq, self.hit_seq, self.midline):
+      # if gap in query sequence
+      if q == '-':
+        h_index += 1
+      # if gap in hit sequence
+      elif h == '-':
+        q_index += 1
+      # if residues align
+      elif m != ' ':
+        query_hit_dict[q_index] = h_index
+        q_index += 1
+        h_index += 1
+      # if residues do not align
+      else:
+        q_index += 1
+        h_index += 1
+
+    return query_hit_dict
--- Project-Code/src/script.py
+++ Project-Code/src/script.py
@@ -2,69 +2,75 @@ import os
 from os.path import dirname, abspath
 import sys
 import textwrap
+
 DIRECTORY = dirname(dirname(abspath(__file__)))
 sys.path.append(DIRECTORY)
-from lib import library 
+from lib import library
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 from Bio import SeqIO
 from Alignment import Alignment
 
-TARGET_DIR = "../data/target/" #place target fasta files here i.e. one fasta file per target protein
+TARGET_DIR = "../data/target/"  # place target fasta files here i.e. one fasta file per target protein
 OUTPUT_DIR = "../data/msa/"
 
 
-def parse_result(msa_xml_file,file_no):
-	result=open(msa_xml_file,"r")
-	output= OUTPUT_DIR+"msa"+str(file_no)+".fasta";
-	records= NCBIXML.parse(result)
-	item=next(records)
+def parse_result(msa_xml_file, file_no):
+	result = open(msa_xml_file, "r")
+	output = OUTPUT_DIR + "msa" + str(file_no) + ".fasta"
+	records = NCBIXML.parse(result)
+	item = next(records)
 	for alignment in item.alignments:
 		for hsp in alignment.hsps:
-			library.write_to_file(output, ">"+alignment.accession[0:4]+":"+alignment.accession[5]+"|PDBID|CHAIN|SEQUENCE","a")
+			library.write_to_file(output, ">" + alignment.accession[0:4] + ":" + alignment.accession[
+				5] + "|PDBID|CHAIN|SEQUENCE", "a")
 			library.write_to_file(output, hsp.query, "a")
-			library.write_to_file(output, hsp.match,"a")
+			library.write_to_file(output, hsp.match, "a")
 			library.write_to_file(output, hsp.sbjct, "a")
 		library.write_to_file(output, "", "a")
-	print(str(file_no)+" msa fasta file has been generated")
+	print(str(file_no) + " msa fasta file has been generated")
 
-def gen_alignment_obj(msa_xml_file):
-	result=open(msa_xml_file,"r")
-	records= NCBIXML.parse(result)
+
+def gen_alignment_list(msa_xml_file):
+	result = open(msa_xml_file, "r")
+	records = NCBIXML.parse(result)
 	alignment_obj_list = []
-	item=next(records)
+	item = next(records)
 	for alignment in item.alignments:
 		for hsp in alignment.hsps:
-			hit_id = alignment.accession[0:4]+"_"+alignment.accession[5]
-			query_range = (hsp.query_start,hsp.query_end)
-			hit_range = (hsp.sbjct_start,hsp.sbjct_end)
-			query_hit_dict = None   #Jeff your code goes in here
-			alignment_obj = Alignment(hit_id, query_range,hit_range,query_hit_dict)
+			hit_id = alignment.accession[0:4] + "_" + alignment.accession[5]
+			query_range = (hsp.query_start, hsp.query_end)
+			hit_range = (hsp.sbjct_start, hsp.sbjct_end)
+			query_seq = hsp.query
+			hit_seq = hsp.sbjct
+			midline = hsp.match
+
+			# dont want native
+			if hit_range == query_range:
+				break
+
+			alignment_obj = Alignment(hit_id, query_range, hit_range, query_seq, hit_seq, midline)
 			alignment_obj_list.append(alignment_obj)
-	return alignment_obj_list;
+
+	return alignment_obj_list
+
 
 def main():
 	library.create_dir(TARGET_DIR)
 	for filename in os.listdir(TARGET_DIR):
-		file_no =0; 
-		if filename.endswith(".fasta") :
-	    	 file_no +=1;
-	    	 target_file = TARGET_DIR+filename;
-	    	 msa_file = OUTPUT_DIR+"msa.xml";
-	    	 record = SeqIO.read(target_file, format="fasta")
-	    	 result_handle = NCBIWWW.qblast("blastp", "pdbaa", record.seq)
-	    	 library.create_dir(OUTPUT_DIR)
-	    	 library.write_stream(msa_file, result_handle)
-	    	 #parse_result(msa_file, file_no)
-	    	 gen_alignment_obj(msa_file)
+		file_no = 0
+		if filename.endswith(".fasta"):
+			file_no += 1
+			target_file = TARGET_DIR + filename
+			msa_file = OUTPUT_DIR + "msa.xml"
+			record = SeqIO.read(target_file, format="fasta")
+			result_handle = NCBIWWW.qblast("blastp", "pdbaa", record.seq)
+			library.create_dir(OUTPUT_DIR)
+			library.write_stream(msa_file, result_handle)
+
+			alignment_list = gen_alignment_list(msa_file)
 		else:
-	        	continue
+			continue
+
 
 main()
-
-# @Jeff code to loop through the object list
-# for i in range(len(align_obj)):
-# 	print(align_obj[i].hit_id)
-# 	print(align_obj[i].query_range) 
-# 	print(align_obj[i].hit_range)
-# 	print(align_obj[i].query_hit_dict)
