fixed gradient descent breaking bond lengths, added variable folding depth, added basic output of PDBs

Project-Code/lib/gradient_descent.py
@@ -33,16 +33,49 @@ def d_normal_d_r(distance_matrix, residue_matrix, r_index):
             d_ij = distance(r1, r2)
             mu_ij = distance_matrix[r_index][i][0]
             sd_ij = distance_matrix[r_index][i][1]
-            d_normal_val = d_normal(d_ij, mu_ij, sd_ij)
-            d_distance_val = d_distance(r1, r2)
-            d_normal_d_r += - np.log(d_normal_val * d_distance_val)
+            # TODO: why are these abs?
+            d_normal_val = np.abs(d_normal(d_ij, mu_ij, sd_ij))
+            d_distance_val = np.abs(d_distance(r1, r2))
+            d_normal_d_r += - np.log(d_normal_val * d_distance_val + 0.0001)
 
     return d_normal_d_r
 
 
+def update_r(a, distance_matrix, residue_matrix, folding_depth):
+    new_residue_matrix = np.copy(residue_matrix)
+
+    # only fold up to given depth
+    for i in range(folding_depth):
+        new_residue_matrix = np.copy(residue_matrix)
+
+        r_update = d_normal_d_r(distance_matrix[:folding_depth,:folding_depth], residue_matrix[:folding_depth], i)
+        gradient_projected_position = - a * r_update + residue_matrix[i]
+        relative_residue_position = residue_matrix[i - 1] if i > 0 else residue_matrix[i + 1]
+        relative_residue_vector = gradient_projected_position - relative_residue_position
+        relative_residue_vector = 3.8 * relative_residue_vector / np.linalg.norm(relative_residue_vector)
+
+        new_residue_matrix[i] = relative_residue_vector + relative_residue_position
+
+        residue_matrix = new_residue_matrix
+
+    # "Pull" residues that have not begun to be optimized yet
+    last_residue = new_residue_matrix[folding_depth - 1]
+    next_residue_vector = last_residue - new_residue_matrix[folding_depth]
+    update_later_residue_vector = next_residue_vector - 3.8 * next_residue_vector / np.linalg.norm(next_residue_vector)
+    new_residue_matrix[folding_depth:] += update_later_residue_vector
+
+    return residue_matrix
+
+
+
+
+
+
 def initialize_residue_matrix(length):
-    residue_matrix = np.zeros((length, 3))
+    residue_matrix = np.ones((length, 3))
     for i in range(length):
-        residue_matrix[i][0] = i * 3.800001
+        residue_matrix[i][0] = i * 3.8
+        residue_matrix[i][1] = i * np.random.normal(loc=0.0001, scale=0.001)
+        residue_matrix[i][2] = i * np.random.normal(loc=0.0001, scale=0.001)
 
     return residue_matrix

Project-Code/src/script.py
@@ -121,6 +121,8 @@ def build_target_distance_pdfs(length, alignment_list):
             if ij_distance_list:
                 target_distance_pdfs[i][j][0] = np.mean(ij_distance_list)
                 target_distance_pdfs[i][j][1] = np.std(ij_distance_list)
+                if len(ij_distance_list) > 0:
+                    target_distance_pdfs[i][j][1] = 8.0
             else:
                 target_distance_pdfs[i][j][0] = np.nan
                 target_distance_pdfs[i][j][1] = np.nan
@@ -153,7 +155,18 @@ def main():
             library.output_distance_matrix(DATA_DIR, distance_matrix[:,:,0])
 
             residue_matrix = gradient_descent.initialize_residue_matrix(len(record.seq))
-            print(gradient_descent.d_normal_d_r(distance_matrix, residue_matrix, 0))
+            new_residue_matrix = residue_matrix.copy()
+            iterations = 20000
+            length = len(record.seq)
+            for i in tqdm(range(iterations)):
+                # update residue matrix for residues up to given depth
+                new_residue_matrix = gradient_descent.update_r(0.0001, distance_matrix, new_residue_matrix,
+                                                               min(length, int((i / iterations) * length + 5)))
+                if i % 1000 == 0:
+                    library.write_pdb(new_residue_matrix, record.seq, "structure-{}".format(i))
+
+            library.write_pdb(new_residue_matrix, record.seq, "final_structure")
+
 
         else:
             continue